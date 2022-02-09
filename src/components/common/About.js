e8d94e0c98535d2fef285c2967b79aa384f74c30
hollypartridge <holpartridge@gmail.com>
2022-02-09 22:34:27 +0000

    about page center and link

--- src/components/common/About.js
+++ src/components/common/About.js
@@ -2,7 +2,12 @@ function About() {
   return (
     <div className="about-page">
       <div className="about-para">
-        <p>Epop is an online community of web developers and designers. Inspired by hoverstat.es, it&#39;s a place where users can come share their work and get inspiration from others. Epop was a group project I created during my time at General Assembly, in collaboration with David Harvey, Harry Murphy and Michael von Etzdorf. The brief was to create a full-stack application in one week. We created this full-stack application using MongoDB, Mongoose, Node.js, Express, React and SCSS. This is the first full-stack app we have built, I hope you enjoy it :&#41; .｡.:*☆</p>
+        <p>Epop is an online community of web developers and designers. Inspired by <a target='_blank'
+          href='https://www.hoverstat.es/'
+          rel='noreferrer'
+        >
+          hoverstat.es
+        </a>, it&#39;s a place where users can come share their work and get inspiration from others. Epop was a group project I created during my time at General Assembly, in collaboration with David Harvey, Harry Murphy and Michael von Etzdorf. The brief was to create a full-stack application in one week. We created this full-stack application using MongoDB, Mongoose, Node.js, Express, React and SCSS. This is the first full-stack app we have built, I hope you enjoy it :&#41; .｡.:*☆</p>
         <p>Find me on <a 
           href='https://github.com/hollypartridge'
           target='_blank'
